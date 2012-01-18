dba3bc7dbdf4e692d87a484170afbdb00c437246
Robert Shippey <robert@robertshippey.net>
2012-01-18 22:11:05 +0000

    Remove Gwin
    Plan to have simple CLI

---
+++ main.cpp
@@ -0,0 +1,17 @@
+//
+//  main.cpp
+//  Ricky
+//
+//  Created for Personal Development 2: Group 2.
+//
+
+#include <iostream>
+
+int main (int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
